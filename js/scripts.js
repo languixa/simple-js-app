8b4dc0ea2b7ca6096035445258e24b45affd07a8
languixa <110312135+languixa@users.noreply.github.com>
2024-02-12 15:50:39 -0800

    added text formatting and animation
    add text formatting:
    -font-weight <b></b>
    -color<span></span>
    
    added text animation:
    ease-in-out infinite

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
+// empty array creation
 let pokemonList = [];
+
+// assigning elements to the array
 pokemonList[0] = {name:"pokeXuxu", height:7, types:['grass', 'poison'],}; 
 pokemonList[1] = {name:"pokeBubu", height:5, types:['rock', 'juice'],}; 
 pokemonList[2] = {name:"pokeTomy", height:10, types:['sand', 'water'],}; 
+
+// for loop containing a conditional 
+for (let i = 0; i < pokemonList.length; i++) {
+    
+    if (pokemonList[i].height>7){
+        document.write("<b><span class='winner'>" + pokemonList[i].name + " (height:" + pokemonList[i].height + ") - Wow, that's big!" + "</b></span>" + "<br>");
+        
+    } else {
+        document.write(pokemonList[i].name + " (height:" + pokemonList[i].height + ")" + "<br>"); 
+    }
+}
