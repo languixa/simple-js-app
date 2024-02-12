c328ead04b7a933bea6e9fc42f2e9626a3f7707f
languixa <110312135+languixa@users.noreply.github.com>
2024-02-12 14:40:02 -0800

    created entirely new js code
    created blank array
    added three new elements: all objects
    each object contains:
    - string
    - integer
    - array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-alert("Hello World");
-let favoriteFood = "orange";
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList[0] = {name:"pokeXuxu", height:7, types:['grass', 'poison'],}; 
+pokemonList[1] = {name:"pokeBubu", height:5, types:['rock', 'juice'],}; 
+pokemonList[2] = {name:"pokeTomy", height:10, types:['sand', 'water'],}; 
