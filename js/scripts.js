c2c3574b5465ce9f248dfb3bb222880d761186d3
languixa <110312135+languixa@users.noreply.github.com>
2024-02-12 20:06:46 -0800

    implemented IIFE
    created a repository variable and assigned IIFE function
    
    updated the for Each loop to getAll keyword

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,35 @@
-// empty array creation
-let pokemonList = [];
 
-// assigning elements to the array
-pokemonList[0] = {name:"pokeXuxu", height:7, types:['grass', 'poison'],}; 
-pokemonList[1] = {name:"pokeBubu", height:5, types:['rock', 'juice'],}; 
-pokemonList[2] = {name:"pokeTomy", height:10, types:['sand', 'water'],}; 
+const pokemonRepository = (() => {
+    let pokemonList = [];
+  
+    const getAll = () => {
+      return pokemonList;
+    };
+  
+    const add = (item) => {
+      pokemonList.push(item);
+    };
+  
+    add({ name: "pokeXuxu", height: 7, types: ['grass', 'poison'] });
+    add({ name: "pokeBubu", height: 5, types: ['rock', 'juice'] });
+    add({ name: "pokeTomy", height: 10, types: ['sand', 'water'] });
+  
+    return {
+      getAll: getAll,
+      add: add
+    };
+  })();
+  
+  // Usage examples:
+  console.log(pokemonRepository.getAll()); // Output: Array containing all the Pokémon objects
+  pokemonRepository.add({ name: "pokeDodo", height: 4, types: ['fire', 'flying'] }); // Adding a new Pokémon
+  console.log(pokemonRepository.getAll()); // Output: Updated array with the new Pokémon
 
-// for loop containing a conditional 
-for (let i = 0; i < pokemonList.length; i++) {
-    
-    if (pokemonList[i].height>7){
-        document.write("<b><span class='winner'>" + pokemonList[i].name + " (height:" + pokemonList[i].height + ") - Wow, that's big!" + "</b></span>" + "<br>");
-        
+// forEach iteration containing a conditional 
+pokemonRepository.getAll().forEach(pokemon => {
+    if (pokemon.height > 7) {
+      document.write("<b><span class='winner'>" + pokemon.name + " (height:" + pokemon.height + ") - Wow, that's big!" + "</b></span>" + "<br>");
     } else {
-        document.write(pokemonList[i].name + " (height:" + pokemonList[i].height + ")" + "<br>"); 
+      document.write(pokemon.name + " (height:" + pokemon.height + ")" + "<br>");
     }
-}
+  });
