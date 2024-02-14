ba7e14c8d143c1c8f50cf50c9dc6e2c938dde926
languixa <110312135+languixa@users.noreply.github.com>
2024-02-14 12:47:41 -0800

    replaced forEach
    replaced  the forEach function with 2 separate functions:
    - addListItem( )
    - showDetaisl( )

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,57 @@
 const pokemonRepository = (() => {
-    let pokemonList = [];
-  
-    const getAll = () => {
-      return pokemonList;
-    };
-  
-    const add = (item) => {
-      pokemonList.push(item);
-    };
+  let pokemonList = [];
 
+  const getAll = () => {
+    return pokemonList;
+  };
+
+  const add = (item) => {
+    pokemonList.push(item);
+  };
+
+  
+
+  add({ name: "pokeXuxu", height: 7, types: ['grass', 'poison'] });
+  add({ name: "pokeBubu", height: 5, types: ['rock', 'juice'] });
+  add({ name: "pokeTomy", height: 10, types: ['sand', 'water'] });
+
+  return {
+    getAll: getAll,
+    add: add,
     
-  
-    add({ name: "pokeXuxu", height: 7, types: ['grass', 'poison'] });
-    add({ name: "pokeBubu", height: 5, types: ['rock', 'juice'] });
-    add({ name: "pokeTomy", height: 10, types: ['sand', 'water'] });
-  
-    return {
-      getAll: getAll,
-      add: add,
-      
-    };
-  })();
+  };
+})();
 
 
-  pokemonRepository.getAll().forEach((pokemon, index) => {
-    const pokemonListElement = document.querySelector('.pokemon-list');
-   
-    // new
-    const objectArray = pokemonRepository.getAll();
-   
-   
-     let listItem = document.createElement('li');
-   
-   
-     let button = document.createElement('button');
-    
-   
-   
-     button.innerText = pokemon.name; // Set the text of the button to the Pokémon name
-     button.classList.add('btn');
-   
-   
-    // Assign a click event listener to the button
+(function addListItem(pokemon){
+  const objectArray = pokemonRepository.getAll();
+  
+  for (let i = 0; i < objectArray.length; i++) {
+  
+  let listItem = document.createElement('li');
+  let button = document.createElement('button');
+  button.innerText = objectArray[i].name; // Set the text of the button to the Pokémon name
+  button.classList.add('btn');
+  document.body.appendChild(listItem)
+  listItem.appendChild(button); // Append the button element to the list item 
+}
+
+})();
+
+(function showDetails() {
+  const buttons = document.querySelectorAll('button');
+  const objectArray = pokemonRepository.getAll();
+
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+
     button.addEventListener('click', function() {
-     // Display a message identifying the clicked button
-     // document.write(`Button ${index + 1} clicked`);
-     document.getElementById("p1").innerHTML = objectArray[index].name + " " + objectArray[index].height + " " + objectArray[index].types;
-   
-   });
-   
-   
-   
-     listItem.appendChild(button); // Append the button element to the list item
-     pokemonListElement.appendChild(listItem); // Append the list item to the main list
+      // Get the innerText value of the clicked button
+      const buttonText = objectArray[i].name;
+
+      // Write the innerText value of the button to the document
+      document.getElementById("p1").innerHTML = buttonText;
     });
-   
+  }
+})();
+
